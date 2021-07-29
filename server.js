86345eaef13a84fe3af925d69932c690d5b5c97f
Troy Grossi <ttroygrossit@gmail.com>
2021-07-28 17:58:06 -0700

    added log-in and log-out capability, login stored by session cookies

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
